65eef2c5633afaeb777e81a520c358fb2a7ce23e
JoeyShapiro <57827758+JoeyShapiro@users.noreply.github.com>
2021-04-21 16:53:05 -0700

    Update bee.py
    added stickyness to be

--- bee.py
+++ bee.py
@@ -1,8 +1,22 @@
 from scapy.all import *
 
 f = open('log.pcap', 'ab')
+stickyIPs = [] # list is easy
 
 def print_pkt(pkt):
-    wrpcap(f, pkt, append=True)
+    try:
+        source = pkt['IP'].src
+        # check if questionable
+        if((pkt['IP'].dst == '192.168.192.131') and (source not in stickyIPs)):
+            #add to list (make sticky)
+            stickyIPs.append(source)
+            print(source + " is now sticky")
+        if souce in stickyIPs:
+            #monitor any traffic sticky IPs do on network
+            wrpcap(f, pkt, append=True)
+            print("doing something")
+    except:
+        pass
 
+print("scanny network")
 sniff(filter="", prn=print_pkt)
